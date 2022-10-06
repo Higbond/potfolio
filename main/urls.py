e646ac9d2fd8e5a87f43b4669aa72bc22a8de802
Vladimir Bondarenko <higbond91@gmai.com>
2022-10-06 19:07:14 +0300

    all commit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.main, name="homepage"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

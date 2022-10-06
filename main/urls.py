1219334d0cdd27a5d3881792d057070e8125be27
Vladimir Bondarenko <higbond91@gmai.com>
2022-10-06 19:09:19 +0300

    all commit

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('', views.main, name="homepage"),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

6d6dce228bd3445df9e653451576a357dd99a55a
Vladimir Bondarenko <higbond91@gmai.com>
2022-10-06 17:24:25 +0300

    all commit

--- Portfolio_2/settings.py
+++ Portfolio_2/settings.py
@@ -1,5 +1,5 @@
 import os
-from django.contrib import staticfiles
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
